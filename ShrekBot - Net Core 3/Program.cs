94ec574f87a897450b9e308d9e295ed9d8b49d32
Uchenna Onuigbo <uchenna.p.onuigbo@gmail.com>
2021-02-08 10:49:00 -0800

    Add project files.

---
+++ ShrekBot - Net Core 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShrekBot___Net_Core_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
